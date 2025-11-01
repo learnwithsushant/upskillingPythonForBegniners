683fc680e49f81047dab567e4c27635be878549a
Sushant Wadjikar <wadjikarsushant@gmail.com>
2025-11-02 04:38:04 +0530

    task2_few_practice_code

---
+++ PythonBasics/NareshIT_SRK_tasks/task2.py
@@ -0,0 +1,8 @@
+# pie=input("Enter a floating point value")
+# print(f" Life of pie is: {pie}")
+
+# age = 100 -  int(input("Enter your age"))
+# print(f" It would take {age} to reach 100 years")
+
+r = float(input("Enter radius"))
+print(f"Area of circle is {3.14*r*r}")
