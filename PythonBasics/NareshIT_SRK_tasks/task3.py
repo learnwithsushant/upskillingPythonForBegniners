fa2c8531a210384c599dcab4608a96e9ea93343e
Sushant Wadjikar <wadjikarsushant@gmail.com>
2025-11-12 22:20:21 +0530

    addition of list elements

--- PythonBasics/NareshIT_SRK_tasks/task3.py
+++ PythonBasics/NareshIT_SRK_tasks/task3.py
@@ -2,3 +2,15 @@ normString = "python"
 print(normString[::-1])
 reveresedString = "".join(reversed(normString))
 print(reveresedString)
+
+list1 = [1, 2, 3, 4]
+list2 = [5, 6, 7, 8]
+list3 = []  # Initialize an empty list to store the results
+
+# Iterate through the lists using their indices
+for i in range(len(list1)):
+    # Add corresponding elements and append the sum to list3
+    list3.append(list1[i] + list2[i])
+
+print(list3)
+
