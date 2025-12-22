416434796115e37555fdceddace9826bc6b1a550
Sushant Wadjikar <wadjikarsushant@gmail.com>
2025-12-22 23:30:26 +0530

    FileWriteOP

--- PythonBasics/UdemyCourse/section3FileOp.py
+++ PythonBasics/UdemyCourse/section3FileOp.py
@@ -2,3 +2,12 @@ myfile = open('PythonBasics\\UdemyCourse\\Resources\\myFile.txt')
 print(myfile.read())
 myfile.seek(0)
 print(myfile.readlines())
+
+with open('PythonBasics\\UdemyCourse\\Resources\\myFile.txt') as myNewFile:
+    contents = myNewFile.read()
+
+    print(contents)
+
+with open('PythonBasics\\UdemyCourse\\Resources\\myFile.txt',mode='a') as myNewWFile:
+    myNewWFile.write('\n This new line writen in write mode')
+    print(myNewWFile)    
